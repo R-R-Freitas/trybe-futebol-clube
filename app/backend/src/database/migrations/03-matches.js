5e9601981f280f3ccf13fa1057201df7a1fedc64
R-R-Freitas <rodrigoaz8@gmail.com>
2022-07-11 18:00:39 -0300

    feat: migrations

---
+++ app/backend/src/database/migrations/03-matches.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      home_team: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      home_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      away_team: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      away_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      in_progress: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('matches');
+  }
+};
