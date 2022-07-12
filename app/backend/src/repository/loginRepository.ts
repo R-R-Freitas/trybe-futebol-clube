d081f25bc065a8dd65896797f2b442c128995466
R-R-Freitas <rodrigoaz8@gmail.com>
2022-07-12 20:41:31 -0300

    feat: login repository

---
+++ app/backend/src/repository/loginRepository.ts
@@ -0,0 +1,13 @@
+import Users from '../database/models/UserModel';
+import { User, ILoginModel } from '../protocols';
+
+export default class Repository implements ILoginModel {
+  constructor(private model = Users) {
+    this.model = model;
+  }
+
+  async create(email: string): Promise<User> {
+    const user = await this.model.findOne({ where: { email } });
+    return user as User;
+  }
+}
