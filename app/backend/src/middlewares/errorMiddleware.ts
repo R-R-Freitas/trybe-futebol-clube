cb577c62aaa30ec99481fa6d7dc349c199842f0c
R-R-Freitas <rodrigoaz8@gmail.com>
2022-07-12 22:40:47 -0300

    feat: errorMiddleware and custom error class

---
+++ app/backend/src/middlewares/errorMiddleware.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express';
+
+import CustomError from '../utils/CustomError';
+
+function errorMiddleware(err: CustomError, _req: Request, res: Response, _next: NextFunction) {
+  return res.status(err.status).json(err.message);
+}
+
+export default errorMiddleware;
