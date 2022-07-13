db81d829a5d6b2078d3cdcb5cc09b39fed71de17
R-R-Freitas <rodrigoaz8@gmail.com>
2022-07-12 22:55:03 -0300

    fix: errorMiddleware return

--- app/backend/src/middlewares/errorMiddleware.ts
+++ app/backend/src/middlewares/errorMiddleware.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import CustomError from '../utils/CustomError';
 
 function errorMiddleware(err: CustomError, _req: Request, res: Response, _next: NextFunction) {
-  return res.status(err.status).json(err.message);
+  return res.status(err.status).json({ message: err.message });
 }
 
 export default errorMiddleware;
