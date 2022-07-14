69f5eb14715ca3e9c8434ad846826f77868550b7
R-R-Freitas <rodrigoaz8@gmail.com>
2022-07-13 21:44:31 -0300

    feat: teams Controller

---
+++ app/backend/src/controllers/teamsController.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import { ITeamsService } from '../protocols';
+
+export default class TeamsController {
+  constructor(private service: ITeamsService) {
+    this.service = service;
+  }
+
+  async getTeamById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const team = await this.service.getTeamById(parseInt(id, 10));
+      return res.status(200).json(team);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getAllTeams(req: Request, res: Response, next: NextFunction) {
+    try {
+      const teams = await this.service.getAllTeams();
+      return res.status(200).json(teams);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
