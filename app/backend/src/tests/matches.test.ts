54dca2d07e4f7e436a4e1e0533de648698e8f20e
R-R-Freitas <rodrigoaz8@gmail.com>
2022-07-14 23:21:59 -0300

    test: finish match, error handling while adding matches

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -12,101 +12,117 @@ import { Response } from 'superagent';
 chai.use(chaiHttp);
 const { expect } = chai;
 
-const foundMatches = [
-  {
-    "id": 1,
-    "homeTeam": 16,
-    "homeTeamGoals": 1,
-    "awayTeam": 8,
-    "awayTeamGoals": 1,
-    "inProgress": false,
-    "teamHome": {
-      "teamName": "São Paulo"
-    },
-    "teamAway": {
-      "teamName": "Grêmio"
-    }
-  },
-  {
-    "id": 41,
-    "homeTeam": 16,
-    "homeTeamGoals": 2,
-    "awayTeam": 9,
-    "awayTeamGoals": 0,
-    "inProgress": true,
-    "teamHome": {
-      "teamName": "São Paulo"
-    },
-    "teamAway": {
-      "teamName": "Internacional"
-    }
-  }
-];
+// const foundMatches = [
+//   {
+//     "id": 1,
+//     "homeTeam": 16,
+//     "homeTeamGoals": 1,
+//     "awayTeam": 8,
+//     "awayTeamGoals": 1,
+//     "inProgress": false,
+//     "teamHome": {
+//       "teamName": "São Paulo"
+//     },
+//     "teamAway": {
+//       "teamName": "Grêmio"
+//     }
+//   },
+//   {
+//     "id": 41,
+//     "homeTeam": 16,
+//     "homeTeamGoals": 2,
+//     "awayTeam": 9,
+//     "awayTeamGoals": 0,
+//     "inProgress": true,
+//     "teamHome": {
+//       "teamName": "São Paulo"
+//     },
+//     "teamAway": {
+//       "teamName": "Internacional"
+//     }
+//   }
+// ];
 
-const inProgressMatches = [
-  {
-    "id": 41,
-    "homeTeam": 16,
-    "homeTeamGoals": 2,
-    "awayTeam": 9,
-    "awayTeamGoals": 0,
-    "inProgress": true,
-    "teamHome": {
-      "teamName": "São Paulo"
-    },
-    "teamAway": {
-      "teamName": "Internacional"
-    }
-  },
-  {
-    "id": 42,
-    "homeTeam": 6,
-    "homeTeamGoals": 1,
-    "awayTeam": 1,
-    "awayTeamGoals": 0,
-    "inProgress": true,
-    "teamHome": {
-      "teamName": "Ferroviária"
-    },
-    "teamAway": {
-      "teamName": "Avaí/Kindermann"
-    }
-  }
-];
+// const inProgressMatches = [
+//   {
+//     "id": 41,
+//     "homeTeam": 16,
+//     "homeTeamGoals": 2,
+//     "awayTeam": 9,
+//     "awayTeamGoals": 0,
+//     "inProgress": true,
+//     "teamHome": {
+//       "teamName": "São Paulo"
+//     },
+//     "teamAway": {
+//       "teamName": "Internacional"
+//     }
+//   },
+//   {
+//     "id": 42,
+//     "homeTeam": 6,
+//     "homeTeamGoals": 1,
+//     "awayTeam": 1,
+//     "awayTeamGoals": 0,
+//     "inProgress": true,
+//     "teamHome": {
+//       "teamName": "Ferroviária"
+//     },
+//     "teamAway": {
+//       "teamName": "Avaí/Kindermann"
+//     }
+//   }
+// ];
 
-const finishedMatches = [
-  {
-    "id": 1,
-    "homeTeam": 16,
-    "homeTeamGoals": 1,
-    "awayTeam": 8,
-    "awayTeamGoals": 1,
-    "inProgress": false,
-    "teamHome": {
-      "teamName": "São Paulo"
-    },
-    "teamAway": {
-      "teamName": "Grêmio"
-    }
-  },
-  {
-    "id": 2,
-    "homeTeam": 9,
-    "homeTeamGoals": 1,
-    "awayTeam": 14,
-    "awayTeamGoals": 1,
-    "inProgress": false,
-    "teamHome": {
-      "teamName": "Internacional"
-    },
-    "teamAway": {
-      "teamName": "Santos"
-    }
-  }
-];
+// const finishedMatches = [
+//   {
+//     "id": 1,
+//     "homeTeam": 16,
+//     "homeTeamGoals": 1,
+//     "awayTeam": 8,
+//     "awayTeamGoals": 1,
+//     "inProgress": false,
+//     "teamHome": {
+//       "teamName": "São Paulo"
+//     },
+//     "teamAway": {
+//       "teamName": "Grêmio"
+//     }
+//   },
+//   {
+//     "id": 2,
+//     "homeTeam": 9,
+//     "homeTeamGoals": 1,
+//     "awayTeam": 14,
+//     "awayTeamGoals": 1,
+//     "inProgress": false,
+//     "teamHome": {
+//       "teamName": "Internacional"
+//     },
+//     "teamAway": {
+//       "teamName": "Santos"
+//     }
+//   }
+// ];
+
+const correctLoginBody = { email: 'admin@admin.com', password: 'secret_admin' }
 
 const newMatch = {
   homeTeam: 16,
+  awayTeam: 8,
+  homeTeamGoals: 2,
+  awayTeamGoals: 2
+};
+
+const equalTeamsMatch = {
+  homeTeam: 8,
+  awayTeam: 8,
+  homeTeamGoals: 2,
+  awayTeamGoals: 2
+};
+
+const invalidTeamMatch = {
+  homeTeam: 99,
   awayTeam: 8,
   homeTeamGoals: 2,
   awayTeamGoals: 2
@@ -211,11 +227,139 @@ describe('Testa se é possível cadastrar uma partida em andamento', () => {
 
   it('Verifica se a chamada retorna o código de status 201 e uma partida em andamento', async () => {
     chaiHttpResponse = await chai
-       .request(app).post('/matches').send(newMatch);
+      .request(app).post('/login').send(correctLoginBody);
+
+    const token = chaiHttpResponse.body.token;
+
+    chaiHttpResponse = await chai
+       .request(app).post('/matches').send(newMatch).set({ 'authorization': token });
 
     expect(chaiHttpResponse.status).to.be.equal(201);
     expect(chaiHttpResponse.body).to.be.an('object');
     expect(chaiHttpResponse.body).to.have.all.keys(['id', 'homeTeam', 'awayTeam', 'homeTeamGoals', 'awayTeamGoals', 'inProgress']);
   });
+
+});
+
+describe('Testa se é possível encerrar uma partida', () => {
+
+  let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Matches, 'findAll')
+  //     .resolves(finishedMatches as unknown as Matches[]);
+  // });
+
+  // after(()=>{
+  //   (Matches.findAll as sinon.SinonStub).restore();
+  // });
+
+  it('Verifica se a chamada retorna o código de status 200 e a message: Finished', async () => {
+    chaiHttpResponse = await chai
+       .request(app).patch('/matches/41/finish');
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.have.property('message', 'Finished');
+  });
+
+});
+
+describe('Testa se não é possível cadastrar uma partida com dois times iguais', () => {
+
+  let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Matches, 'findAll')
+  //     .resolves(finishedMatches as unknown as Matches[]);
+  // });
+
+  // after(()=>{
+  //   (Matches.findAll as sinon.SinonStub).restore();
+  // });
+
+  it('Verifica se a chamada retorna o código de status 401 e a mensagem de erro', async () => {
+    chaiHttpResponse = await chai
+      .request(app).post('/login').send(correctLoginBody);
+
+    const token = chaiHttpResponse.body.token;
+
+    chaiHttpResponse = await chai
+       .request(app).post('/matches').send(equalTeamsMatch).set({ 'authorization': token });
+
+    const errorMessage = 'It is not possible to create a match with two equal teams'
+
+    expect(chaiHttpResponse.status).to.be.equal(401);
+    expect(chaiHttpResponse.body).to.have.property('message', errorMessage);  });
+
+});
+
+describe('Testa se não é possível cadastrar uma partida com um time inexistente', () => {
+
+  let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Matches, 'findAll')
+  //     .resolves(finishedMatches as unknown as Matches[]);
+  // });
+
+  // after(()=>{
+  //   (Matches.findAll as sinon.SinonStub).restore();
+  // });
+
+  it('Verifica se a chamada retorna o código de status 401 e a mensagem de erro', async () => {
+    chaiHttpResponse = await chai
+      .request(app).post('/login').send(correctLoginBody);
+
+    const token = chaiHttpResponse.body.token;
+
+    chaiHttpResponse = await chai
+       .request(app).post('/matches').send(invalidTeamMatch).set({ 'authorization': token });
+
+    const errorMessage = 'There is no team with such id!'
+
+    expect(chaiHttpResponse.status).to.be.equal(401);
+    expect(chaiHttpResponse.body).to.have.property('message', errorMessage);  });
+
+});
+
+describe('Testa se não é possível cadastrar uma partida sem um token válido', () => {
+
+  let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Matches, 'findAll')
+  //     .resolves(finishedMatches as unknown as Matches[]);
+  // });
+
+  // after(()=>{
+  //   (Matches.findAll as sinon.SinonStub).restore();
+  // });
+
+  it('Verifica se a chamada com token inválido retorna o código de status 401 e a mensagem de erro', async () => {
+    const token = 'token_inválido';
+
+    chaiHttpResponse = await chai
+       .request(app).post('/matches').send(newMatch).set({ 'authorization': token });
+
+    const errorMessage = 'Token must be a valid token';
+
+    expect(chaiHttpResponse.status).to.be.equal(401);
+    expect(chaiHttpResponse.body).to.have.property('message', errorMessage);
+  });
+
+    it('Verifica se a chamada sem um token retorna o código de status 401 e a mensagem de erro', async () => {
+  
+      chaiHttpResponse = await chai
+         .request(app).post('/matches').send(newMatch);
+  
+      const errorMessage = 'Token must be a valid token';
+  
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body).to.have.property('message', errorMessage);
+    });
 
 });
