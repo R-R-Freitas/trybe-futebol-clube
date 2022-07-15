251b03eeaa4fd2bf9dfb16155fd1dd12cbe2de0a
R-R-Freitas <rodrigoaz8@gmail.com>
2022-07-14 23:54:30 -0300

    feat: added verification for equal teams

--- app/backend/src/services/matchesService.ts
+++ app/backend/src/services/matchesService.ts
@@ -1,3 +1,4 @@
+import CustomError from '../utils/CustomError';
 import { IMatchesService, IMatchesModel, MatchAndTeams, Match, NewMatch } from '../protocols';
 
 export default class MatchesService implements IMatchesService {
@@ -16,6 +17,9 @@ export default class MatchesService implements IMatchesService {
   }
 
   async createMatch(newMatch: NewMatch): Promise<Match> {
+    const { homeTeam, awayTeam } = newMatch;
+    const equalTeamsError = 'It is not possible to create a match with two equal teams';
+    if (homeTeam === awayTeam) throw new CustomError(equalTeamsError, 401);
     const match = await this.model.createMatch(newMatch);
     return match;
   }
